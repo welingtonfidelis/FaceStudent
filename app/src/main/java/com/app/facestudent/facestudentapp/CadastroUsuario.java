458218ce5bf93da864731bf84def0749de9abf50
welingtonfidelis@gmail.com <Evolucao1990>
2018-10-30 14:16:33 -0300

    Tela cadastro usuario iniciada

---
+++ app/src/main/java/com/app/facestudent/facestudentapp/CadastroUsuario.java
@@ -0,0 +1,13 @@
+package com.app.facestudent.facestudentapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuario);
+    }
+}
--- app/src/main/java/com/app/facestudent/facestudentapp/Login.java
+++ app/src/main/java/com/app/facestudent/facestudentapp/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -22,6 +23,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
@@ -61,7 +63,7 @@ public class Login extends AppCompatActivity {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
 
                 if(user != null){
-
+                    Log.e("FIREBASEUSER", "nada");
                 }
                 else{
 
@@ -91,45 +93,22 @@ public class Login extends AppCompatActivity {
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 firebaseAuthWithGoogle(account);
-
-                String key = account.getEmail();//guarda email como chave unica do usuario
-                key = key.replace(".","*"); //substitui ponto por asterisco (padrao firebase reserva ponto final)
-
-                buscaUsuarioBanco(key);
-                if(usuario == null){
-                    //inicio a tela de cadastro
-                    usuario = new Usuario();
-                    ReferencesHelper.getDatabaseReference().child("Usuario").child(key).setValue(usuario);
-                    Toast.makeText(this, "Usuario nao cadastrado", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    //inicio tela de listar areas
-                    Toast.makeText(this, "Usuario ja cadastrado", Toast.LENGTH_SHORT).show();
-                }
-
             } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
+                Log.e("SIGINERROR", e.getMessage());
             }
         }
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingl    mAuth.addAuthStateListener(mAuthListiner);
-    }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onDestroy() {
+        super.onDestroy();
         if(mAuthListiner != null){
             mAuth.removeAuthStateListener(mAuthListiner);
         }
     }
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-       // Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -140,6 +119,25 @@ public class Login extends AppCompatActivity {
                            // Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
 
+                            ReferencesHelper.getDatabaseReference().child("Usuario").child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    if(dataSnapshot.exists()){
+                                        Intent intent = new Intent(Login.this, ListaArea.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }else{
+                                        Intent intent = new Intent(Login.this, CadastroUsuario.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
                         } else {
                             // If sign in fails, display a message to the user.
                             //Log.w(TAG, "signInWithCredential:failure", task.getException());
@@ -149,24 +147,4 @@ public class Login extends AppCompatActivity {
                 });
     }
 
-    public void buscaUsuarioBanco(String email){
-        usarioEventListener = new ValueEventListener() {
-            @Override
-            public void onDataChange( com.google.firebase.database.DataSnapshot dataSnapshot) {
-
-                if(dataSnapshot.exists()){
-                    usuario = new Usuario();
-                    usuario = dataSnapshot.getValue(Usuario.class);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-
-        };
-
-        ReferencesHelper.getDatabaseReference().child("Usuario").child(email).addValueEventListener(usarioEventListener);
-    }
 }
